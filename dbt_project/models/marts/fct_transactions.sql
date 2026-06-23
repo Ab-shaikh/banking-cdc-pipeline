0ed1bb53dac4e7761b4169d69ec1d321e7ec86c7
Ab-shaikh <shaikhabubakar059@gmail.com>
2026-06-23 22:38:57 +0530

    fix: resolve dbt 2.0 validation errors and deduplicate fct_transactions using qualify

--- dbt_project/models/marts/fct_transactions.sql
+++ dbt_project/models/marts/fct_transactions.sql
@@ -1,4 +1,8 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='incremental',
+    unique_key='transaction_id',
+    incremental_strategy='merge'
+) }}
 
 SELECT
     transaction_id,
@@ -13,3 +17,14 @@ SELECT
         ELSE 'VALID'
     END AS data_quality_flag
 FROM {{ ref('stg_transactions') }}
+
+{% if is_incremental() %}
+  -- Incremental filter for performance
+  WHERE transaction_time > (SELECT MAX(transaction_time) FROM {{ this }})
+{% endif %}
+
+-- Is ek line se batch ke saare duplicates filter ho jayenge!
+QUALIFY ROW_NUMBER() OVER (
+    PARTITION BY transaction_id 
+    ORDER BY transaction_time DESC
+) = 1
